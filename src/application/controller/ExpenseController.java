f388c1d327a2fc55d0d750ea8d6af33da460140f
Ian Uriegas <ianuriegas@ians-air.lan>
2022-11-23 19:08:01 -0600

    made add/remove buttons and text fields for user inputs

--- src/application/controller/ExpenseController.java
+++ src/application/controller/ExpenseController.java
@@ -10,36 +10,55 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 public class ExpenseController {
 
-    @FXML
-    private Label total_expense_label;
+	@FXML
+	private TextField input_amount;
 
-    @FXML
-    private Button expense_button;
+	@FXML
+	private Label total_expense_label;
 
-    @FXML
-    private Button profit_button;
+	@FXML
+	private TextField input_frequency;
 
-    @FXML
-    private Button expense_profit_button;
+	@FXML
+	private TextField input_name;
 
-    @FXML
-    private TableView<?> expense_profit_table;
+	@FXML
+	private Button add_button;
 
-    @FXML
-    private Label username_label;
+	@FXML
+	private Button remove_button;
 
-    @FXML
-    void expense_profit_button_clicked(ActionEvent event) {
-    	try {
+	@FXML
+	private TextField inputt_date;
+
+	@FXML
+	private Button expense_button;
+
+	@FXML
+	private Button profit_button;
+
+	@FXML
+	private Button expense_profit_button;
+
+	@FXML
+	private TableView<?> expense_profit_table;
+
+	@FXML
+	private Label username_label;
+
+	@FXML
+	void expense_profit_button_clicked(ActionEvent event) {
+		try {
 
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(getClass().getResource("../view/main.fxml"));
 
 			Scene scene = new Scene(loader.load());
-			
+
 			Main.stage.setScene(scene);
 			Main.stage.show();
 
@@ -47,22 +66,22 @@ public class ExpenseController {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    }
+	}
 
-    @FXML
-    void expense_button_clicked(ActionEvent event) {
+	@FXML
+	void expense_button_clicked(ActionEvent event) {
 
-    }
+	}
 
-    @FXML
-    void profit_button_clicked(ActionEvent event) {
-    	try {
+	@FXML
+	void profit_button_clicked(ActionEvent event) {
+		try {
 
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(getClass().getResource("../view/profit.fxml"));
 
 			Scene scene = new Scene(loader.load());
-			
+
 			Main.stage.setScene(scene);
 			Main.stage.show();
 
@@ -70,6 +89,16 @@ public class ExpenseController {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    }
+	}
+
+	@FXML
+	void add_button_clicked(ActionEvent event) {
+
+	}
+
+	@FXML
+	void remove_button_clicked(ActionEvent event) {
+
+	}
 
 }
--- src/application/controller/ProfitController.java
+++ src/application/controller/ProfitController.java
@@ -10,36 +10,55 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 public class ProfitController {
 
-    @FXML
-    private Label total_expense_label;
+	@FXML
+	private TextField input_amount;
 
-    @FXML
-    private Button expense_button;
+	@FXML
+	private Label total_expense_label;
 
-    @FXML
-    private Button profit_button;
+	@FXML
+	private TextField input_frequency;
 
-    @FXML
-    private Button expense_profit_button;
+	@FXML
+	private TextField input_name;
 
-    @FXML
-    private TableView<?> expense_profit_table;
+	@FXML
+	private Button add_button;
 
-    @FXML
-    private Label username_label;
+	@FXML
+	private Button remove_button;
 
-    @FXML
-    void expense_profit_button_clicked(ActionEvent event) {
-    	try {
+	@FXML
+	private TextField inputt_date;
+
+	@FXML
+	private Button expense_button;
+
+	@FXML
+	private Button profit_button;
+
+	@FXML
+	private Button expense_profit_button;
+
+	@FXML
+	private TableView<?> expense_profit_table;
+
+	@FXML
+	private Label username_label;
+
+	@FXML
+	void expense_profit_button_clicked(ActionEvent event) {
+		try {
 
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(getClass().getResource("../view/main.fxml"));
 
 			Scene scene = new Scene(loader.load());
-			
+
 			Main.stage.setScene(scene);
 			Main.stage.show();
 
@@ -47,17 +66,17 @@ public class ProfitController {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    }
+	}
 
-    @FXML
-    void expense_button_clicked(ActionEvent event) {
-    	try {
+	@FXML
+	void expense_button_clicked(ActionEvent event) {
+		try {
 
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(getClass().getResource("../view/expense.fxml"));
 
 			Scene scene = new Scene(loader.load());
-			
+
 			Main.stage.setScene(scene);
 			Main.stage.show();
 
@@ -65,11 +84,21 @@ public class ProfitController {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    }
+	}
 
-    @FXML
-    void profit_button_clicked(ActionEvent event) {
+	@FXML
+	void profit_button_clicked(ActionEvent event) {
 
-    }
+	}
+
+	@FXML
+	void add_button_clicked(ActionEvent event) {
+
+	}
+
+	@FXML
+	void remove_button_clicked(ActionEvent event) {
+
+	}
 
 }
